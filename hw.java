386fdfd379d6915a63543bbd6b3e1018fe97f02d
Faraz Yar Khan <47003252+farazyarkhan@users.noreply.github.com>
2021-07-01 09:58:49 -0700

    Update hw.java

--- hw.java
+++ hw.java
@@ -1,5 +1,8 @@
-class HelloWorld {
+class Sum {
     public static void main(String[] args) {
-        System.out.println("Hello, World! This is first change"); 
+        int num1 = 5, num2 = 15, sum;
+        sum = num1 + num2;
+
+        System.out.println("Sum of these numbers: "+sum); 
     }
 }
